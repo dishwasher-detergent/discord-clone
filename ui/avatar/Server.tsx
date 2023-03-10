601fabbc1877b95d7c8c359afa6efef64a15f8a6
dishwasher-detergent <kenny_bass@outlook.com>
2023-03-09 18:38:38 -0600

    Update avatars

--- ui/avatar/Server.tsx
+++ ui/avatar/Server.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import * as Tooltip from "@radix-ui/react-tooltip";
 import { motion, MotionConfig } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
@@ -24,40 +25,55 @@ export default function ServerAvatar({
 
   return (
     <MotionConfig transition={{ duration: 0.15 }}>
-      <li
-        className="w-full grid place-items-center relative"
-        onMouseEnter={() => setHovered(true)}
-        onMouseLeave={() => setHovered(false)}
-      >
-        <div className="absolute left-0 h-full flex items-center">
-          <motion.div
-            className="rounded-r-full bg-slate-900 dark:bg-slate-50"
-            initial={false}
-            animate={{
-              width: hovered ? ".25rem" : 0,
-              height: hovered ? "1.5rem" : 0,
-            }}
-          />
-        </div>
-        <motion.div
-          className="bg-slate-900 aspect-square cursor-pointer dark:bg-slate-50 overflow-hidden relative"
-          style={{ width: width, height: height }}
-          initial={false}
-          animate={{
-            borderRadius: hovered ? "24px" : "32px",
-          }}
-        >
-          <Link href={`/channel/${server}`}>
-            {src ? (
-              <Image src={src} alt={title} fill />
-            ) : (
-              <div className="font-bold w-full h-full text-xl grid place-items-center capitalize">
-                {title.slice(0, 2)}
+      <Tooltip.Provider delayDuration={0}>
+        <Tooltip.Root>
+          <Tooltip.Trigger asChild>
+            <li
+              className="w-full grid place-items-center relative"
+              onMouseEnter={() => setHovered(true)}
+              onMouseLeave={() => setHovered(false)}
+            >
+              <div className="absolute left-0 h-full flex items-center">
+                <motion.div
+                  className="rounded-r-full bg-slate-900 dark:bg-slate-50"
+                  initial={false}
+                  animate={{
+                    width: hovered ? ".25rem" : 0,
+                    height: hovered ? "1.5rem" : 0,
+                  }}
+                />
               </div>
-            )}
-          </Link>
-        </motion.div>
-      </li>
+              <motion.div
+                className="bg-slate-900 aspect-square cursor-pointer dark:bg-slate-50 overflow-hidden relative"
+                style={{ width: width, height: height }}
+                initial={false}
+                animate={{
+                  borderRadius: hovered ? "24px" : "32px",
+                }}
+              >
+                <Link href={`/channel/${server}`}>
+                  {src ? (
+                    <Image src={src} alt={title} fill />
+                  ) : (
+                    <div className="font-bold w-full h-full text-xl grid place-items-center capitalize">
+                      {title.slice(0, 2)}
+                    </div>
+                  )}
+                </Link>
+              </motion.div>
+            </li>
+          </Tooltip.Trigger>
+          <Tooltip.Portal>
+            <Tooltip.Content
+              side="right"
+              className="shadow-lg z-[9999] py-1 px-2.5 rounded-xl bg-white text-slate-900 dark:bg-slate-900 dark:text-white text-sm font-bold"
+            >
+              {title}
+              <Tooltip.Arrow />
+            </Tooltip.Content>
+          </Tooltip.Portal>
+        </Tooltip.Root>
+      </Tooltip.Provider>
     </MotionConfig>
   );
 }
--- ui/avatar/User.tsx
+++ ui/avatar/User.tsx
@@ -19,7 +19,7 @@ export default function Avatar({
       style={{ width: width, height: height }}
     >
       <div className="bg-slate-900 rounded-full h-full w-full aspect-square dark:bg-slate-50 overflow-hidden relative">
-        <Image src={src} alt={title} fill />
+        {/* <Image src={src} alt={title} fill /> */}
       </div>
       <div className="z-40 w-4 h-4 absolute bottom-0 right-0 rounded-full border-2 border-slate-100 dark:border-slate-600 bg-green-400" />
     </div>
